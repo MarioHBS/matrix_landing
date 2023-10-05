c10fba7e0767af03cdb44880347bcf5f3ace8c81
Mário Henrique <marioh90@gmail.com>
2023-10-05 20:31:19 -0300

    adicionadas imagens de Animatrix; adicionado container em Movies para conter a animação; adicionados elementos <span> por meio do JS; animações com @keyFrame; muitos ajustes em 'movies.scss' para efeitos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 const classMore = '.movie__more-details';
 
 document.addEventListener('DOMContentLoaded', () => {
+    addingElementsInMovies();
+
     const btMores = document.querySelectorAll(classMore);
     // console.log(btMores);
 
@@ -14,4 +16,11 @@ function toggleOpenMore() {
     console.log('click more');
     const change = 'movie__more-details--is-open';
     this.classList.toggle(change);
+}
+
+function addingElementsInMovies() {
+    const node = document.querySelector('.movies__container');
+    for (let i = 0; i < 249; i++) { // adicionar 260 spans
+        node.insertAdjacentHTML('beforebegin', '<span class="movies__spans"></span>');
+    }
 }
